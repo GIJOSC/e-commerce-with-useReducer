7094e7fa4dc9dfa8a884507a899662220338ad3d
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-11 02:44:29 -0300

    fix/ fixed products array in context

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,9 +1,32 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import faker from "faker";
 
 const Cart = createContext();
+faker.seed(100);
 
 const Context = ({ children }) => {
-  return <Cart.Provider>{children}</Cart.Provider>;
+  const [cart, setCart] = useState([]);
+
+  const productsArray = [...Array(20)].map((p) => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.random.image(),
+    inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+    fastDelivery: faker.datatype.boolean(),
+  }));
+
+  const [products] = useState(productsArray);
+
+  return (
+    <Cart.Provider value={{ cart, setCart, products }}>
+      {children}
+    </Cart.Provider>
+  );
+};
+
+export const CartState = () => {
+  return useContext(Cart);
 };
 
 export default Context;
